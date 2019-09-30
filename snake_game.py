116e2737553d936b6e3d88909425c5474f2a9e59
markgmz92 <markgmz92@gmail.com>
2019-10-01 01:20:40 +0800

    Created snake head and movement of snake head

--- snake_game.py
+++ snake_game.py
@@ -22,11 +22,46 @@ head.direction = "up"
 # Functions
 
 
+def go_up():
+    head.direction = "up"
+
+
+def go_down():
+    head.direction = "down"
+
+
+def go_left():
+    head.direction = "left"
+
+
+def go_right():
+    head.direction = "right"
+
+
 def move():
     if head.direction == "up":
         y = head.ycor()
         head.sety(y + 20)
 
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
 
 # Main game loop
 while True:
