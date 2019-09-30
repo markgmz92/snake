27a3fb9b6c4442bd142489069b0adf11ec45eaf8
markgmz92 <markgmz92@gmail.com>
2019-10-01 00:56:49 +0800

    Include time module

--- snake_game.py
+++ snake_game.py
@@ -1,4 +1,7 @@
 import turtle
+import time
+
+delay = 0.1
 
 # Set up the screen
 wn = turtle.Screen()
@@ -6,5 +9,30 @@ wn.title("Snake")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
 wn.tracer(0)
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0, 0)
+head.direction = "up"
+
+# Functions
+
+
+def move():
+    if head.direction == "up":
+	y = head.ycor()
+	head.sety(y + 20)
+
+
+# Main game loop
+while True:
+    wn.update()
+
+    move()
+    time.sleep(delay)
 
 wn.mainloop()
